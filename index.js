f0b9e2a231085d5bc130599a83b57b989e718c01
unknown <muhammadabdullateef211@gmail.com>
2026-05-21 20:22:19 +0100

    Updated Navbar

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-  const menuIcon = document.querySelector(".fa-bars");
-      const dropDownMenu = document.querySelector(".dropdown");
-      const homeSection = document.getElementById("home-section");
-      const submitComplaint = document.getElementById("submit-complaint");
-      const submit = document.querySelectorAll(".submit");
-      const home = document.querySelector(".hom");
+const menuIcon = document.querySelector(".fa-bars");
+const dropDownMenu = document.querySelector(".dropdown");
+const homeSection = document.getElementById("home-section");
+const submitComplaint = document.getElementById("submit-complaint");
+const submit = document.querySelectorAll(".submit");
+const home = document.querySelector(".hom");
+const hom = document.querySelector(".home");
+const body = document.body;
+const sumit = document.querySelector(".sumit");
 
-      submit.forEach((button) => {
-        button.addEventListener("click", () => {
-          homeSection.style.display = "none";
-          submitComplaint.style.display = "block";
-        });
-      });
+function setDisplay(el1, el2) {
+  el1.style.display = "none";
+  el2.style.display = "block";
+}
 
-      home.addEventListener("click", () => {
-        homeSection.style.display = "block";
-        submitComplaint.style.display = "none";
-      });
+submit.forEach((button) => {
+  button.addEventListener("click", () => {
+    setDisplay(homeSection, submitComplaint);
+    sumit.id = "active";
+    hom.id = "";
+  });
+});
 
-      menuIcon.addEventListener("click", (e) => {
-        dropDownMenu.classList.toggle("open");
-        if (dropDownMenu.classList.contains("open")) {
-          menuIcon.classList.add("fa-x");
-          menuIcon.classList.remove("fa-bars");
-        } else {
-          menuIcon.classList.remove("fa-x");
-          menuIcon.classList.add("fa-bars");
-        }
-        // e.stopPropagation();
-      });
-      document.addEventListener("click", (e) => {
-        if (!menuIcon.contains(e.target) && !dropDownMenu.contains(e.target)) {
-          dropDownMenu.classList.remove("open");
-          menuIcon.classList.remove("fa-x");
-          menuIcon.classList.add("fa-bars");
-        }
-      });
+home.addEventListener("click", () => {
+  setDisplay(submitComplaint, homeSection);
+  hom.id = "active";
+  sumit.id = "";
+});
 
-      
+menuIcon.addEventListener("click", () => {
+  dropDownMenu.classList.toggle("open");
+  if (dropDownMenu.classList.contains("open")) {
+    menuIcon.classList.add("fa-x");
+    menuIcon.classList.remove("fa-bars");
+  } else {
+    menuIcon.classList.remove("fa-x");
+    menuIcon.classList.add("fa-bars");
+  }
+  // e.stopPropagation();
+});
+document.addEventListener("click", (e) => {
+  if (!menuIcon.contains(e.target) && !dropDownMenu.contains(e.target)) {
+    dropDownMenu.classList.remove("open");
+    menuIcon.classList.remove("fa-x");
+    menuIcon.classList.add("fa-bars");
+  }
+});
