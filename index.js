2e4f7ea39f9ecb9fd9e3999a18dec93c86db5b4e
unknown <muhammadabdullateef211@gmail.com>
2026-06-08 01:13:49 +0100

    Update

--- index.js
+++ index.js
@@ -2,30 +2,54 @@ const menuIcon = document.querySelector(".fa-bars");
 const dropDownMenu = document.querySelector(".dropdown");
 const homeSection = document.getElementById("home-section");
 const submitComplaint = document.getElementById("submit-complaint");
-const submit = document.querySelectorAll(".submit");
+const submit = document.querySelector(".submit-home");
+const track = document.querySelector(".track-home")
 const home = document.querySelector(".hom");
 const hom = document.querySelector(".home");
 const body = document.body;
 const sumit = document.querySelector(".sumit");
+const items = document.querySelectorAll(".item");
+const sections = document.querySelectorAll(".section");
 
 function setDisplay(el1, el2) {
   el1.style.display = "none";
   el2.style.display = "block";
 }
 
-submit.forEach((button) => {
-  button.addEventListener("click", () => {
-    setDisplay(homeSection, submitComplaint);
-  sumit.classList.add("active");
-  hom.classList.remove("active")
-  });
-});
+function switchSection(index){
+  items.forEach((item) => {
+    item.classList.remove("active");
+  })
+      sections.forEach((section) => {
+        section.style.display = "none";
+      });
+      items[index].classList.add("active");
+      sections[index].style.display = "block";
+}
 
-home.addEventListener("click", () => {
-  setDisplay(submitComplaint, homeSection);
-  hom.classList.add("active");
-  sumit.classList.remove("active")
-});
+
+items.forEach((item, index) => {
+  item.addEventListener(
+    "click", () =>{
+      switchSection(index)
+    }
+  )
+})
+
+submit.addEventListener("click", () =>{
+  switchSection(1)
+})
+
+track.addEventListener("click", () =>{
+  switchSection(2)
+})
+
+
+// home.addEventListener("click", () => {
+//   setDisplay(submitComplaint, homeSection);
+//   hom.classList.add("active");
+//   sumit.classList.remove("active");
+// });
 
 menuIcon.addEventListener("click", () => {
   dropDownMenu.classList.toggle("open");
